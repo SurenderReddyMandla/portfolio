df5a1fc3335e59abb6a4c15fa65a92f3a4905999
surender reddy mandla <surenderreddy891@gmail.com>
2020-05-30 22:54:23 -0400

    version 5.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,7 +53,7 @@ class Homepage extends React.Component {
                                 <h2>Experience</h2>
                             </header>
                             <p><strong>COMPANY:&nbsp;&nbsp;<a href="https://www.archents.com/" target="_blank"> Archents</a></strong></p>
-                            <p>I Worked here as a software developer and done <br/> projects based on Web development and also worked on Blockchain Technology and Robotic Process Automation</p><br/>
+                            <p>I worked here as a software developer and done <br/> projects based on Web development and also worked on Blockchain Technology and Robotic Process Automation</p><br/>
                             <p><strong>COMPANY:&nbsp;&nbsp;<a href="https://www.fanshawec.ca/" target="_blank"> Fanshawe College</a></strong></p>
                             <p>I worked as a contract tutor in the department of Electronics and Embedded systems development</p>
                             <header className="major">
